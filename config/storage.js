6bd706ce078f5c9fdf25ff23557446880a92ffc0
unknown <chandanpms@gmail.com>
2025-09-16 14:52:31 +0530

    updated

--- config/storage.js
+++ config/storage.js
@@ -4,7 +4,7 @@ const cloudinary = require("./cloudinary");
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
-    folder: "blogs", // Optional folder name
+    folder: "kpd", // Optional folder name
     allowed_formats: ["jpg", "png", "jpeg", "webp"],
   },
 });
